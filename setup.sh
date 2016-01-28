63406ff22337515d63ba15adde56584e05233cc4
pl3bs <brandonchapman@gmx.com>
2016-01-28 15:43:52 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 echo "11 3   *   *   * root    cd ~/den && ./exitor.sh" >> /etc/crontab ;
+cd /root/den
 wget https://raw.githubusercontent.com/pl3bs/denyexitnodes/master/exitor.sh ;
 chmod +x exitor.sh
 ./exitor.sh ;
