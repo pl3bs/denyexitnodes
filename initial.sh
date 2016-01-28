07875e2fc5c413676e826ca617b005ecfe4a8244
pl3bs <brandonchapman@gmx.com>
2016-01-28 15:26:50 -0600

    Update initial.sh
    error line 3

--- initial.sh
+++ initial.sh
@@ -1,4 +1,4 @@
 echo "11 3   *   *   * root    cd ~/den && ./exitor.sh" >> /etc/crontab ;
 wget https://raw.githubusercontent.com/pl3bs/denyexitnodes/master/exitor.sh ;
-chmox +x exitor.sh
+chmod +x exitor.sh
 ./exitor.sh ;
